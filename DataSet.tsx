5982af5469deaeb3d231fd68250fd237f02f951f
huhrray <huhsey@gmail.com>
2022-03-09 18:18:11 +0900

    Set up commonly used variables -styles, dataset

---
+++ DataSet.tsx
@@ -0,0 +1,30 @@
+
+
+export const carouselData = [
+    {
+        title: '고구미',
+        source: require('./assets/snacks/Carousel1.jpg')
+    }
+    ,
+    {
+        title: '시금치 aleksi is the best',
+        source: require('./assets/snacks/Carousel2.jpg')
+    }
+    ,
+    {
+        title: '치즈볼',
+        source: require('./assets/snacks/Carousel3.jpg')
+    }
+
+]
+
+export const shopCategory = [
+    { title: "닭", key: 'item1' },
+    { title: "오리", key: 'item2' },
+    { title: "돼지, 소", key: 'item3' },
+    { title: "특수고기", key: 'item4' },
+    { title: "수산물", key: 'item5' },
+    { title: "채소, 농산물", key: 'item6' },
+    { title: "파우더", key: 'item7' },
+    { title: "자연식, 베이커리", key: 'item8' }
+]
--- functions/style.tsx
+++ functions/style.tsx
@@ -1,4 +1,10 @@
 import { Dimensions } from 'react-native';
 
-export const screenWidth = Dimensions.get('screen').width
-export const screenHeight = Dimensions.get('screen').height
+export const windowWidth = Dimensions.get('window').width
+export const windowHeight = Dimensions.get('window').height
+export const mainColor = "#5b473c"
+export const subColor = "#c1b9b4"
+export const oatmeal = "#f0edec"
+export const brightBrown = "#7f5d4d"
+export const maingFont = "Gugi-Regular"
+export const subFont = "GowunDodum-Regular"
